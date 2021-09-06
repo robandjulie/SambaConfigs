ef17636c94d37acf2a158f022864267c2d14021e
Rob Dumas <robdumas@gmail.com>
2021-09-06 12:08:41 -0500

    Updated to reflect my own info architecture.

--- scripts/create_user_share.sh
+++ scripts/create_user_share.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ ! -e /tank/user/$1 ]; then
-	zfs create tank/user/$1
-	chown $1:"shares" /tank/user/$1
-    chmod -R 700 $1 /tank/user/$1
-    ln -s /tank/public /tank/user/$1/Public
+USERSFOLDER='/srv/trunk/home'
+GROUP=nihilsum
+
+if [ ! -e $USERSFOLDER/$1 ]; then
+	zfs create $USERSFOLDER/$1
+	chown $1:$GROUP $USERSFOLDER/$1
+    chmod -R 700 $1 $USERSFOLDER/$1
+    # ln -s /tank/public $USERSFOLDER/$1/Public
 fi
 exit 0
--- scripts/create_user_time_machine.sh
+++ scripts/create_user_time_machine.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ ! -e /tank/time_machine/$1 ]; then
-	mkdir /tank/time_machine/$1
-	chown $1:"time_machine" /tank/time_machine/$1
-    chmod -R 700 $1 /tank/time_machine/$1
+TMFOLDER="/srv/trunk/srv/timecapsule"
+
+if [ ! -e $TMFOLDER/$1 ]; then
+	mkdir $TMFOLDER/$1
+	chown $1:"time_machine" $TMFOLDER/$1
+    chmod -R 700 $1 $TMFOLDER/$1
 fi
 exit 0
